12dbaccc19702027e90027888378bd69c8a10951
WhaleMonster <xuboxes@gmail.com>
2015-02-12 14:55:59 -0800

    added env file

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,11 +3,11 @@ get '/' do
 end
 
 get '/hero/:letter' do
-	marvel = Comic.new(ENV['PUBLIC'], ENV['PRIVATE'], params[:letter])
+	marvel = Comic.new('1c7104a208178203bdba77263604da6e', '0ab00e5a35c17bc30d20cdc09eecf9e92fb95fa8', params[:letter])
 	marvel.get_chars['data']['results'].to_json
 end
 
 get '/hero/search/:id' do
-	marvel = Comic.new(ENV['PUBLIC'], ENV['PRIVATE'], params[:letter], params[:id])
+	marvel = Comic.new('1c7104a208178203bdba77263604da6e', '0ab00e5a35c17bc30d20cdc09eecf9e92fb95fa8', params[:letter], params[:id])
 	marvel.get_specific_char['data']['results'].to_json
 end
